e681ad7f9382274177704a95cba80197f3eaa792
BedivereZero <BedivereZero@gmail.com>
2021-10-14 18:04:05 +0800

    feat(huawei-sdk-go) add list elb pool

---
+++ huawei-sdk-go/list-elb-pool/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/auth/basic"
+	elb "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/elb/v2"
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/elb/v2/model"
+	region "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/elb/v2/region"
+)
+
+func main() {
+	ak := os.Getenv("HUAWEI_ACCESS_KEY_ID")
+	sk := os.Getenv("HUAWEI_SECRET_ACCESS_KEY_ID")
+
+	auth := basic.NewCredentialsBuilder().
+		WithAk(ak).
+		WithSk(sk).
+		Build()
+
+	client := elb.NewElbClient(
+		elb.ElbClientBuilder().
+			WithRegion(region.ValueOf("cn-east-3")).
+			WithCredential(auth).
+			Build())
+
+	request := &model.ListPoolsRequest{}
+	nameRequest := "server_group-test-elb-bedivere"
+	request.Name = &nameRequest
+	response, err := client.ListPools(request)
+	if err == nil {
+		fmt.Printf("%+v\n", response)
+	} else {
+		fmt.Println(err)
+	}
+}
