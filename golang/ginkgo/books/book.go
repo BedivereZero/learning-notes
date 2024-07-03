db893acbbc6add063a562989fa55254e64394904
BedivereZero <BedivereZero@gmail.com>
2024-07-03 18:11:40 +0800

    golang/ginkgo: Organizing Specs With Container Nodes
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- golang/ginkgo/books/book.go
+++ golang/ginkgo/books/book.go
@@ -1,9 +1,26 @@
 package books
 
+import "strings"
+
 type Book struct {
 	Title  string `json:"title,omitempty"`
 	Author string `json:"author,omitempty"`
 	Pages  int    `json:"pages,omitempty"`
+}
+
+func (b *Book) IsValid() bool {
+	var conditions = []bool{
+		b.Title != "",
+		b.Author != "",
+		b.Pages > 0,
+	}
+	for _, c := range conditions {
+		if c {
+			continue
+		}
+		return false
+	}
+	return true
 }
 
 type Category string
@@ -18,4 +35,22 @@ func (b *Book) Category() Category {
 		return CategoryShortStory
 	}
 	return CategoryNovel
+}
+
+func (b *Book) AuthorFirstName() string {
+	before, _, found := strings.Cut(b.Author, " ")
+	if !found {
+		return ""
+	}
+
+	return before
+}
+
+func (b *Book) AuthorLastName() string {
+	_, after, found := strings.Cut(b.Author, " ")
+	if !found {
+		return b.Author
+	}
+
+	return after
 }
--- golang/ginkgo/go.mod
+++ golang/ginkgo/go.mod
@@ -3,8 +3,11 @@ module github.com/BedivereZero/learning-notes/golang/ginkgo
 go 1.22.4
 
 require (
+	github.com/go-logr/zapr v1.3.0
 	github.com/onsi/ginkgo/v2 v2.19.0
 	github.com/onsi/gomega v1.33.1
+	go.uber.org/zap v1.27.0
+	k8s.io/klog/v2 v2.130.1
 )
 
 require (
@@ -12,6 +15,7 @@ require (
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/pprof v0.0.0-20240424215950-a892ee059fd6 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
